696bcb5e2504660cb3f349bfa6c572688d64ca63
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-09 18:59:20 +0300

    Created preliminary app structure. Implemented routing for it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TestingComponent } from './testing/testing.component';
+import { DatabaseComponent } from './database/database.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: '/testing', pathMatch: 'full'},
+    {path: 'testing', component: TestingComponent},
+    {path: 'database', component: DatabaseComponent},
+    {path: 'settings', component: SettingsComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: '**', redirectTo: '/not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestingComponent } from './testing/testing.component';
+import { DatabaseComponent } from './database/database.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestingComponent,
+    DatabaseComponent,
+    NotFoundComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
