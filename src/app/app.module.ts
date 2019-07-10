6422c34c2d5beef906f3514445c41c8395819c2a
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-10 18:45:59 +0300

    Added basic navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TestingComponent } from './testing/testing.component';
 import { DatabaseComponent } from './database/database.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SettingsComponent } from './settings/settings.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SettingsComponent } from './settings/settings.component';
     TestingComponent,
     DatabaseComponent,
     NotFoundComponent,
-    SettingsComponent
+    SettingsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
