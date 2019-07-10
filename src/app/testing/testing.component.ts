661c8d3b1d1635212dfd525d0995e79eeb22a80a
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-10 18:46:44 +0300

    Now redirecting form Testing to Database at the start, while there is nothing in Testing to see

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-testing',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./testing.component.css']
 })
 export class TestingComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+    constructor(private router: Router) {}
+    ngOnInit() {
+        // XXX: temporary, while there is nothing in Testing to see
+        this.router.navigate(['/database']); 
+    }
 }
