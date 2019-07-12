782835bcdeb1567c5eb45b4d36f32bfc633c49ec
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-12 18:22:59 +0300

    Added basic structure for words dictionary

--- src/app/words.model.ts
+++ src/app/words.model.ts
@@ -1,20 +1,41 @@
-export class Words {
-    content = [ // from
-        [ // from 0
-            {},
-            {}
-        ],
-        [ // from 1
-            {},
-            {}
-        ]
-    ];
-
-    from() {
-
+export class Source {
+    constructor(public language: string, public destinations: Destination[]) {}
+    isFor(language: string) {
+        return this.language == language;
     }
 
-    to() {
+    // destinationsFor(language: string) {
+    //     for (let destination of this.destinations) {
+    //         if (destination.isFor(language)) {
+    //             return destination;
+    //         }
+    //     }
+    // }
+}
 
+export class Destination {
+    constructor(public language: string, public wordsList: WordEntry[]) {}
+    isFor(language: string) {
+        return this.language == language;
+    }
+}
+
+export class Words {
+    sources: Source[];
+
+    from(language: string): Destination[] {
+        for (let source of sources) {
+            if (source.isFor(language)) {
+                return source.destinations;
+            }
+        }
+    }
+
+    to(language: string): WordEntry[] {
+        for (let destination of destinations) {
+            if (destination.isFor(language)) {
+                return destination.wordsList;
+            }
+        }
     }
 }
