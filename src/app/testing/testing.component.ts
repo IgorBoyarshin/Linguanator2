fb6091bc493896c33ce2547ebb0cdb0c6d54e8fe
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-23 16:25:14 +0300

    Introduced Testing state

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -7,14 +7,20 @@ import { WordsDatabaseService } from '../words-database.service';
 import { SettingsService } from '../settings.service';
 import { DataProviderFactoryService } from '../data-provider-factory.service';
 
+enum State {
+    UserInput,
+    DisplayResult,
+}
+
 @Component({
     selector: 'app-testing',
     templateUrl: './testing.component.html',
     styleUrls: ['./testing.component.css']
 })
 export class TestingComponent implements OnInit {
-    wordEntry: WordEntry;
-    languagePair: LanguagePair;
+    private wordEntry: WordEntry;
+    private languagePair: LanguagePair;
+    private state: State;
 
     private languageIndexer: LanguageIndexer;
 
@@ -26,6 +32,7 @@ export class TestingComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.state = State.UserInput;
         this.languagePair = this.settingsService.languagePairInUse();
         const randomWord = this.wordsDatabaseService.randomWordEntryFor(this.languagePair);
         if (randomWord) {
@@ -39,6 +46,14 @@ export class TestingComponent implements OnInit {
     }
 
     submitInput() {
+        this.state = State.DisplayResult;
+    }
 
+    submitButtonText(): string {
+        return (this.state == State.UserInput) ? "Submit" : "Next";
+    }
+
+    textareaDisabled(): boolean {
+        return this.state != State.UserInput;
     }
 }
