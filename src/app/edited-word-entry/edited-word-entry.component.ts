5a0e42ee2b66694afa3c462acb00812f95f3100e
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-23 18:27:02 +0300

    Prepared for submitting

--- src/app/edited-word-entry/edited-word-entry.component.ts
+++ src/app/edited-word-entry/edited-word-entry.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-edited-word-entry',
-  templateUrl: './edited-word-entry.component.html',
-  styleUrls: ['./edited-word-entry.component.css']
+    selector: 'app-edited-word-entry',
+    templateUrl: './edited-word-entry.component.html',
+    styleUrls: ['./edited-word-entry.component.css']
 })
 export class EditedWordEntryComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+    word: string = "";
+    translations: string = "";
+    tags: string = "";
+
+    constructor() {}
+    ngOnInit() {}
+
+    canSubmit(): boolean {
+        return (this.word.trim().length > 0) && (this.translations.trim().length > 0);
+    }
+
+    submit() {
+        const translations = this.translations.split(';');
+        const tags = this.tags.length == 0 ? [] : this.tags.split(';');
+        // TODO
+    }
 }
