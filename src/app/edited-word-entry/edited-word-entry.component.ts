c4fe3c0e045d871f6bd381175562ddeee391463e
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-07-23 18:15:37 +0300

    Created basic design

---
+++ src/app/edited-word-entry/edited-word-entry.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edited-word-entry',
+  templateUrl: './edited-word-entry.component.html',
+  styleUrls: ['./edited-word-entry.component.css']
+})
+export class EditedWordEntryComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
