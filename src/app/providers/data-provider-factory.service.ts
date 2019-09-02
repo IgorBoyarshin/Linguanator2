1b78a1e592d1890ee1d67933cbfe48a5d236c6af
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-09-02 17:14:24 +0300

    Resurrected StaticDataProvider

--- src/app/providers/data-provider-factory.service.ts
+++ src/app/providers/data-provider-factory.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { DataProvider } from './data-provider';
-// import { StaticDataProvider } from './static-data-provider';
+import { StaticDataProvider } from './static-data-provider';
 import { HttpDataProvider } from './http-data-provider';
 
 @Injectable({
@@ -17,8 +17,8 @@ export class DataProviderFactoryService {
     public dataProviderInUse(): DataProvider {
         if (!this.dataProvider) {
             // DataProvider to be used throughout the application is set here
-            // this.dataProvider = new StaticDataProvider();
-            this.dataProvider = new HttpDataProvider(this.http);
+            this.dataProvider = new StaticDataProvider();
+            // this.dataProvider = new HttpDataProvider(this.http);
         }
         return this.dataProvider;
     }
--- src/app/providers/static-data-provider.ts
+++ src/app/providers/static-data-provider.ts
@@ -1,127 +1,202 @@
-// import { Observable } from 'rxjs';
-// import { of } from 'rxjs';
-//
-// import { DataProvider } from './data-provider';
-// import { WordEntry } from '../word-entry.model';
-// import { Language } from '../language-model';
-// import { LanguageIndexer } from '../language-indexer';
-//
-// export class StaticDataProvider implements DataProvider {
-//     private nextWordEntryIdToUse: number = 1;
-//     private entries: WordEntry[];
-//     private languageIndexer: LanguageIndexer;
-//
-//     constructor() {}
-//
-//     retrieveEntries(): Observable<WordEntry[]> {
-//         if (!this.entries) {
-//             return Observable.create(subscriber => {
-//                 this.retrieveLanguageIndexer().subscribe(languageIndexer => {
-//                     this.entries = this.initEntries(languageIndexer);
-//                     this.nextWordEntryIdToUse = 1 + this.maxIdUsed(this.entries);
-//                     subscriber.next(this.entries);
-//                 });
-//             });
-//         }
-//         return of(this.entries);
-//     }
-//
-//     retrieveLanguageIndexer(): Observable<LanguageIndexer> {
-//         if (!this.languageIndexer) {
-//             this.languageIndexer = this.initLanguageIndexer();
-//         }
-//         return of(this.languageIndexer);
-//     }
-//
-//     addWordEntry({from, to, word, translations, score, tags}: WordEntry): Observable<void> {
-//         return Observable.create(subscriber => {
-//             const DEFAULT_TAGS = ['main'];
-//             if (tags.length == 0) tags = DEFAULT_TAGS;
-//             // const currentUserId = this.authService.current
-//             const currentUserId = 1; // TODO
-//             this.entries.push(new WordEntry(currentUserId, this.nextWordEntryIdToUse, from, to, word, translations, score, tags));
-//             this.nextWordEntryIdToUse++;
-//             subscriber.next();
-//         });
-//     }
-//
-//     updateWordEntry(id: number, newEntry: WordEntry): Observable<void> {
-//         return Observable.create(subscriber => {
-//             const index = this.indexOfEntryWithId(id, this.entries);
-//             if (index == -1) return; // not found : (
-//             this.entries[index] = newEntry;
-//
-//             subscriber.next();
-//         });
-//     }
-//
-//     removeWordEntry(id: number): Observable<void> {
-//         return Observable.create(subscriber => {
-//             const index = this.indexOfEntryWithId(id, this.entries);
-//             if (index == -1) return; // not found : (
-//             this.entries.splice(index, 1);
-//
-//             subscriber.next();
-//         });
-//     }
-//
-//     private indexOfEntryWithId(id: number, entries: WordEntry[]): number {
-//         for (let i = 0; i < entries.length; i++) {
-//             if (entries[i].id === id) return i;
-//         }
-//         console.error("[indexOfEntryWithId()]: not found for id=", id);
-//         return -1;
-//     }
-//
-//     private initEntries(languageIndexer: LanguageIndexer): WordEntry[] {
-//         const ger = languageIndexer.indexOf("German");
-//         const eng = languageIndexer.indexOf("English");
-//         const userId = 1;
-//         let nextId = 1;
-//         return [
-//             new WordEntry(userId, nextId++, ger, eng, 'weit', ['wide', 'far', 'broad'], 2, ['tag1', 'tag2']),
-//             new WordEntry(userId, nextId++, ger, eng, 'aufmerksam', ['attentive', 'mindful', 'thoughtful'], 0, ['tag1']),
-//             new WordEntry(userId, nextId++, ger, eng, 'gehorsam', ['obedient', 'submissive'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'die Einsamkeit', ['the loneliness', 'the solitude'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'regungslos', ['motionless', 'dead still'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'die Kerze', ['the candle'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'zu zweit', ['in pairs'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'der Fels', ['the rock', 'the cliff'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'kundtun', ['make known', 'proclaim'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'das Verlangen', ['the demand', 'the desire', 'the request'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'verderben', ['spoil', 'ruin', 'corrupt'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'wesentlich', ['significant', 'essential', 'crucial', 'fundamental'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'die Sünde', ['the sin'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'auswendig', ['by heart'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'der Schatz', ['the treasure', 'the sweetheart', 'the honey'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'anschauen', ['look at', 'watch', 'view'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'unbeteiligt', ['unconcerned', 'uninvolved', 'indifferent'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'beweisen', ['prove', 'demonstrate', 'show'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'leuchten', ['light', 'shine', 'glow'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'allerdings', ['however', 'certainly', 'admittedly', 'though'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'sich mit etwas begnügen', ['content oneself with something', 'make do with something'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'ungefähr', ['approximate', 'rough', 'nearly'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'bestätigen', ['confirm', 'acknowledge', 'verify'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'die Verachtung', ['the disdain', 'the disgust'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'zur selben Zeit', ['at the same time'], 1, ['main']),
-//             new WordEntry(userId, nextId++, ger, eng, 'das Gedicht', ['the poem', 'the ode'], 1, ['main']),
-//
-//             new WordEntry(userId, nextId++, eng, ger, 'the boy', ['der Junge', 'der Knabe'], 3, ['main']),
-//         ];
-//     }
-//
-//     private initLanguageIndexer(): LanguageIndexer {
-//         const languages = [
-//             new Language(1, "English"),
-//             new Language(2, "German"),
-//         ];
-//         return new LanguageIndexer(languages);
-//     }
-//
-//     private maxIdUsed(entries: WordEntry[]): number {
-//         if (entries.length == 0) return -1;
-//         const ids = entries.map(word => word.id);
-//
-//         let maxId = ids[0];
-//     }
-// }
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+import { DataProvider } from './data-provider';
+import { WordEntry } from '../word-entry.model';
+import { Language } from '../language-model';
+import { LanguagePair } from '../language-pair.model';
+import { LanguageIndexer } from '../language-indexer';
+
+import * as Tags from '../auth/local-storage-tags';
+
+
+class User {
+    constructor(public id: number, public username, public password) {}
+}
+
+class UserWithEntries {
+    constructor(public id: number, public username, public password, public wordEntries: WordEntry[]) {}
+}
+
+
+export class StaticDataProvider implements DataProvider {
+    private nextWordEntryIdToUse: number;
+    private entries: WordEntry[];
+    private languageIndexer: LanguageIndexer;
+
+    private users: UserWithEntries[];
+
+    public retrieveEntries(): Observable<WordEntry[]> {
+        if (!this.entries) {
+            return Observable.create(subscriber => {
+                this.retrieveLanguageIndexer().subscribe(languageIndexer => {
+                    const allEntries = this.initEntries(languageIndexer);
+                    this.nextWordEntryIdToUse = this.nextAvailableId(allEntries);
+                    this.users = this.wordEntriesIntoUsers(allEntries);
+                    const targetUsername = this.currentUser();
+                    const { id } = this.users.find(({ username }) => username === targetUsername);
+                    this.entries = this.wordEntriesForUserId(id)
+                    subscriber.next(this.entries);
+                });
+            });
+        }
+        return of(this.entries);
+    }
+
+    public retrieveLanguageIndexer(): Observable<LanguageIndexer> {
+        if (!this.languageIndexer) {
+            this.languageIndexer = this.initLanguageIndexer();
+        }
+        return of(this.languageIndexer);
+    }
+
+    public addWordEntry({ src, dst }: LanguagePair,
+                        word: string,
+                        translations: string[],
+                        tags: string[]): Observable<void> {
+        return Observable.create(subscriber => {
+            const DEFAULT_TAGS = ['main'];
+            if (tags.length == 0) tags = DEFAULT_TAGS;
+
+            const targetUsername = this.currentUser();
+            const { id } = this.users.find(({ username }) => username === targetUsername);
+
+            const score = 0;
+            this.entries.push(new WordEntry(id, this.nextWordEntryIdToUse,
+                                src, dst, word, translations, score, tags));
+            this.nextWordEntryIdToUse++;
+            subscriber.next();
+        });
+    }
+
+    public updateWordEntry(id: number,
+                           { src, dst }: LanguagePair,
+                           word: string,
+                           translations: string[],
+                           score: number,
+                           tags: string[]): Observable<void> {
+        return Observable.create(subscriber => {
+            const index = this.indexOfEntryWithId(id, this.entries);
+            if (index != -1) {
+                const ref = this.entries[index];
+                ref.id = id;
+                ref.from = src;
+                ref.to = dst;
+                ref.word = word;
+                ref.translations = translations;
+                ref.score = score;
+                ref.tags = tags;
+            }
+            subscriber.next();
+        });
+    }
+
+    public removeWordEntry(id: number): Observable<void> {
+        return Observable.create(subscriber => {
+            const index = this.indexOfEntryWithId(id, this.entries);
+            if (index != -1) this.entries.splice(index, 1);
+            subscriber.next();
+        });
+    }
+
+    private indexOfEntryWithId(id: number, entries: WordEntry[]): number {
+        for (let i = 0; i < entries.length; i++) {
+            if (entries[i].id === id) return i;
+        }
+        console.error("[indexOfEntryWithId()]: not found for id=", id);
+        return -1;
+    }
+
+    private initEntries(languageIndexer: LanguageIndexer): WordEntry[] {
+        const ger = languageIndexer.indexOf("German");
+        const eng = languageIndexer.indexOf("English");
+        const userId1 = 1;
+        const userId2 = 2;
+        let id = 1;
+        return [
+            new WordEntry(userId1, id++, ger, eng, 'weit', ['wide', 'far', 'broad'], 3, ['tag1', 'tag2']),
+            new WordEntry(userId1, id++, ger, eng, 'aufmerksam', ['attentive', 'mindful', 'thoughtful'], 1, ['tag1']),
+            new WordEntry(userId1, id++, ger, eng, 'gehorsam', ['obedient', 'submissive'], 2, ['tag2']),
+            new WordEntry(userId1, id++, ger, eng, 'die Einsamkeit', ['the loneliness', 'the solitude'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'regungslos', ['motionless', 'dead still'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'die Kerze', ['the candle'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'zu zweit', ['in pairs'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'der Fels', ['the rock', 'the cliff'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'kundtun', ['make known', 'proclaim'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'das Verlangen', ['the demand', 'the desire', 'the request'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'verderben', ['spoil', 'ruin', 'corrupt'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'wesentlich', ['significant', 'essential', 'crucial', 'fundamental'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'die Sünde', ['the sin'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'auswendig', ['by heart'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'der Schatz', ['the treasure', 'the sweetheart', 'the honey'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'anschauen', ['look at', 'watch', 'view'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'unbeteiligt', ['unconcerned', 'uninvolved', 'indifferent'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'beweisen', ['prove', 'demonstrate', 'show'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'leuchten', ['light', 'shine', 'glow'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'allerdings', ['however', 'certainly', 'admittedly', 'though'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'sich mit etwas begnügen', ['content oneself with something', 'make do with something'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'ungefähr', ['approximate', 'rough', 'nearly'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'bestätigen', ['confirm', 'acknowledge', 'verify'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'die Verachtung', ['the disdain', 'the disgust'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'zur selben Zeit', ['at the same time'], 2, ['main']),
+            new WordEntry(userId1, id++, ger, eng, 'das Gedicht', ['the poem', 'the ode'], 2, ['main']),
+
+            new WordEntry(userId1, id++, eng, ger, 'the boy', ['der Junge', 'der Knabe'], 4, ['main']),
+
+            new WordEntry(userId2, id++, ger, eng, 'der Vogel', ['the bird'], 3, ['joetag']),
+            new WordEntry(userId2, id++, eng, ger, 'the cow', ['die Kuh'], 4, ['joetag']),
+            new WordEntry(userId2, id++, eng, ger, 'the horse', ['das Pferd'], 5, ['main'])
+        ];
+    }
+
+    private initLanguageIndexer(): LanguageIndexer {
+        const languages = [
+            new Language(1, "English"),
+            new Language(2, "German"),
+        ];
+        return new LanguageIndexer(languages);
+    }
+
+    private nextAvailableId(entries: WordEntry[]): number {
+        if (entries.length == 0) return 1;
+        const ids = entries.map(entry => entry.id);
+
+        let maxId = ids[0];
+        for (let id of ids) {
+            if (id > maxId) maxId = id;
+        }
+
+        return maxId + 1;
+    }
+
+    private currentUser(): string {
+        return localStorage.getItem(Tags.USERNAME)
+    }
+
+    private wordEntriesIntoUsers(wordEntries: WordEntry[]): UserWithEntries[] {
+        let usersWithEntries = [];
+        const users = this.initUsers();
+        for (let { id, username, password } of users) {
+            usersWithEntries.push(new UserWithEntries(id, username, password, []));
+        }
+
+        for (let wordEntry of wordEntries) {
+            const userId = wordEntry.userId;
+            const { wordEntries } = usersWithEntries.find(({ id }) => id == userId);
+            wordEntries.push(wordEntry);
+        }
+
+        return usersWithEntries;
+    }
+
+    private wordEntriesForUserId(userId: number): WordEntry[] {
+        return this.users.find(({ id }) => id == userId).wordEntries;
+    }
+
+    private initUsers(): User[] {
+        return [
+            new User(1, 'Igorek', 'igorekpass'),
+            new User(2, 'Joe', 'joepass')
+        ];
+    }
+}
